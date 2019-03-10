26727168a6a663b404f28cde9e96eb9da47de38f
Eshu Thakur (Ujjwal Jadoun) <37092836+ZadeThakur@users.noreply.github.com>
2019-03-10 15:27:44 -0230

    Updates

---
+++ precache-manifest.f847f0136f44a065b5f6b0a1d9ffcf0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57446a73a386c4e4bd72",
+    "url": "/react-language-translator/static/js/runtime~main.e9034ca1.js"
+  },
+  {
+    "revision": "c2124161c27ce949041c",
+    "url": "/react-language-translator/static/js/main.0f4d6d6c.chunk.js"
+  },
+  {
+    "revision": "e3d022058730ad1dc0e6",
+    "url": "/react-language-translator/static/js/2.2970f048.chunk.js"
+  },
+  {
+    "revision": "c2124161c27ce949041c",
+    "url": "/react-language-translator/static/css/main.6d7766e3.chunk.css"
+  },
+  {
+    "revision": "5139d62d9ffb15f603bf0af7bd197407",
+    "url": "/react-language-translator/index.html"
+  }
+];
